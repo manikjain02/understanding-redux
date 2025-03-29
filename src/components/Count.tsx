4d9a85ee04cdb33dfb2389dbd69704fed19ec791
Manik Jain <manik.jain02@gmail.com>
2025-03-30 00:44:08 +0530

    understanding the use of createSelector

--- src/components/Count.tsx
+++ src/components/Count.tsx
@@ -1,16 +1,18 @@
 import { useSelector } from "react-redux";
+import { getCountSelector } from "../redux/store";
 
-interface ICountStateType {
-  countSlice: {
-    count: number;
-  };
-}
+// interface ICountStateType {
+//   countSlice: {
+//     count: number;
+//   };
+// }
 
 const Count = () => {
-  const { count } = useSelector((state: ICountStateType) => state?.countSlice);
+  // const { count } = useSelector((state: ICountStateType) => state?.countSlice);
+  const countData = useSelector(getCountSelector);
   return (
     <div>
-      <h3>Count: {count}</h3>
+      <h3>Count: {countData.count}</h3>
     </div>
   );
 };
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,8 +1,13 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, createSelector } from "@reduxjs/toolkit";
 import { counterSlice } from "./slices/counterSlice";
 
 const store = configureStore({
   reducer: { countSlice: counterSlice.reducer },
 });
+
+export const getCountSelector = createSelector(
+  (state) => state?.countSlice,
+  (state) => state
+);
 
 export { store };
